abfa8b99622a8e8837854d8416cb6f4f370a93a6
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-08 18:33:14 +0300

    Add product favorite card

---
+++ lib/view/layout/favorite/widget/product_favorite_widget.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../helpers/theme/app_colors.dart';
+import '../../../../helpers/theme/app_text_style.dart';
+import '../../../custom_widgets/custom_image/custom_network_image.dart';
+import '../controller/favorite_controller.dart';
+import '../model/product_favorite_model.dart';
+
+class ProductFavoriteWidget extends StatelessWidget {
+  final ProductFavoriteModel product;
+
+  const ProductFavoriteWidget({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.whiteColor,
+        borderRadius: BorderRadius.circular(18),
+        border: Border.all(color: AppColors.borderColor),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.vertical(top: Radius.circular(18)),
+                child: CustomNetworkImage(
+                  imageUrl: product.productImage ?? '',
+                  height: 145,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                top: 8,
+                right: 8,
+                child: InkWell(
+                  onTap: () => context.read<FavoriteController>().addOrRemoveProductFavorite(id: product.id ?? 0),
+                  child: const CircleAvatar(
+                    radius: 18,
+                    backgroundColor: Colors.white,
+                    child: Icon(Icons.favorite_rounded, color: Colors.red, size: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  product.productName ?? product.productTitle ?? '',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: AppTextStyle.text16MS(),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  product.resturantName ?? '',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: AppTextStyle.text13RG(),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  '${product.productPrice ?? 0} جنيه',
+                  style: AppTextStyle.text17MS().copyWith(color: AppColors.mainAppColor),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

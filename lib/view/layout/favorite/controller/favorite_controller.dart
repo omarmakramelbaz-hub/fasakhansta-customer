abe01f1a827b0360b62df27ed98fbeba7955a0f0
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-08 18:34:44 +0300

    Connect favorites screen to product wishlist API

--- lib/view/layout/favorite/controller/favorite_controller.dart
+++ lib/view/layout/favorite/controller/favorite_controller.dart
@@ -4,7 +4,7 @@ import '../../../../helpers/networking/api_helper.dart';
 import '../../../../helpers/networking/urls.dart';
 import '../../../../helpers/utils/common_methods.dart';
 import '../../../../helpers/utils/utils.dart';
-import '../model/product_favorite_model.dart';
+import '../model/favorite_model.dart';
 
 class FavoriteController extends ChangeNotifier {
   void initialFavorite() {
@@ -16,8 +16,8 @@ class FavoriteController extends ChangeNotifier {
   ApiResponse _favoriteResponse = ApiResponse(state: ResponseState.sleep, data: null);
   ApiResponse get favoriteResponse => _favoriteResponse;
 
-  List<ProductFavoriteModel> _favorite = [];
-  List<ProductFavoriteModel> get favorite => _favorite;
+  List<FavoriteModel> _favorite = [];
+  List<FavoriteModel> get favorite => _favorite;
 
   Future<void> getFavorite() async {
     _favoriteResponse = ApiResponse(state: ResponseState.loading, data: null);
@@ -29,13 +29,13 @@ class FavoriteController extends ChangeNotifier {
     if (_favoriteResponse.state == ResponseState.complete) {
       final data = _favoriteResponse.data['data'];
       if (data is Iterable) {
-        _favorite = data.map((e) => ProductFavoriteModel.fromJson(e)).toList();
+        _favorite = data.map((e) => FavoriteModel.fromJson(e)).toList();
       }
     }
     notifyListeners();
   }
 
-  Future<void> addOrRemoveProductFavorite({required int id}) async {
+  Future<void> addOrRemoveToWishlist({required int id, required VoidCallback onSuccess}) async {
     Utils.loading();
     final response = await ApiHelper.instance.post(Urls.toggleProductFavorite(id));
     Utils.loadingOff();
@@ -43,9 +43,14 @@ class FavoriteController extends ChangeNotifier {
     if (response.state == ResponseState.complete) {
       CommonMethods.showToast(message: response.data['message']);
       _favorite.removeWhere((item) => item.id == id);
+      onSuccess.call();
       notifyListeners();
     } else {
       CommonMethods.showError(message: response.data['message'], apiResponse: response);
     }
+  }
+
+  Future<void> addOrRemoveProductFavorite({required int id}) async {
+    await addOrRemoveToWishlist(id: id, onSuccess: () {});
   }
 }

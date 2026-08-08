3ea67695012d5ec5b5f8404ab5e43b2754030cb8
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-08 19:52:43 +0300

    fix: add missing text12RG style

--- lib/helpers/theme/app_text_style.dart
+++ lib/helpers/theme/app_text_style.dart
@@ -175,6 +175,10 @@ class AppTextStyle {
     return TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: color ?? AppColors.secondAppColor);
   }
 
+  static TextStyle text12RG({Color? color}) {
+    return TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: color ?? AppColors.greyColor);
+  }
+
   static TextStyle text12MS({Color? color}) {
     return TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: color ?? AppColors.secondAppColor);
   }

449939581740ac58395402ed3383d56e7eaf565e
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-08 17:48:35 +0300

    Redesign cart checkout action bar

--- lib/view/layout/cart/widgets/button_nav_cart_widget.dart
+++ lib/view/layout/cart/widgets/button_nav_cart_widget.dart
@@ -8,31 +8,92 @@ class ButtonNavCartWidget extends StatelessWidget {
   final Function()? onPressedExecuteTheOrder;
   final Function()? onTapAddItems;
   final String totalInCart;
-  const ButtonNavCartWidget({super.key, this.onPressedExecuteTheOrder, this.onTapAddItems, required this.totalInCart});
+
+  const ButtonNavCartWidget({
+    super.key,
+    this.onPressedExecuteTheOrder,
+    this.onTapAddItems,
+    required this.totalInCart,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onPressedExecuteTheOrder,
+    return SafeArea(
+      top: false,
       child: Container(
-        height: 74,
+        padding: const EdgeInsets.fromLTRB(14, 10, 14, 12),
         decoration: BoxDecoration(
           color: AppColors.whiteColor,
-          boxShadow: [BoxShadow(color: Colors.grey.withValues(alpha: 0.5), blurRadius: 10, offset: const Offset(0, 0))],
-        ),
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-        child: Container(
-          decoration: BoxDecoration(color: AppColors.mainAppColor, borderRadius: BorderRadius.circular(10)),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('continueToPayment'.tr, style: AppTextStyle.text16BW()),
-                Text('pound'.tr.replaceAll('{}', totalInCart), style: AppTextStyle.text16BW()),
-              ],
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(22)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.10),
+              blurRadius: 16,
+              offset: const Offset(0, -4),
             ),
-          ),
+          ],
+        ),
+        child: Row(
+          textDirection: TextDirection.rtl,
+          children: [
+            Expanded(
+              child: SizedBox(
+                height: 56,
+                child: ElevatedButton(
+                  onPressed: onPressedExecuteTheOrder,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.mainAppColor,
+                    foregroundColor: AppColors.whiteColor,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'continueToPayment'.tr,
+                        style: AppTextStyle.text16BW(),
+                      ),
+                      const SizedBox(width: 8),
+                      const Icon(Icons.arrow_back_ios_new_rounded, size: 18),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Container(
+              height: 56,
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                color: AppColors.whiteColor,
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(
+                  color: AppColors.mainAppColor.withValues(alpha: 0.45),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'total'.tr,
+                    style: AppTextStyle.text12RG().copyWith(
+                      color: AppColors.greyColor,
+                    ),
+                  ),
+                  const SizedBox(height: 2),
+                  Text(
+                    'pound'.tr.replaceAll('{}', totalInCart),
+                    style: AppTextStyle.text14BS().copyWith(
+                      color: AppColors.mainAppColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );

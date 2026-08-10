e667284d40763b167e6cbf8b07ef14bbc9638017
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-10 03:33:04 +0300

    Fix Flutter Web startup by skipping mobile notification initialization

--- lib/helpers/app/init_services.dart
+++ lib/helpers/app/init_services.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -12,9 +13,18 @@ import '../utils/http_overrides.dart';
 
 Future<void> initServices() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await FirebaseNotifications.setUpFirebase();
+
+  // The current notification implementation uses dart:io/mobile-only APIs.
+  // Skip its initialization on Flutter Web so app startup does not crash there.
+  if (!kIsWeb) {
+    await FirebaseNotifications.setUpFirebase();
+  }
+
   await Hive.initFlutter();
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   await Hive.openBox('app');
   await GlobalTranslations.init();

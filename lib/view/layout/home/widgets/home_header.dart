eebe6538c65d7a422fad5bc247ec71d4a0cb07b0
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-10 19:22:22 +0300

    Fix web header build error

--- lib/view/layout/home/widgets/home_header.dart
+++ lib/view/layout/home/widgets/home_header.dart
@@ -173,10 +173,12 @@ class _SeaHeaderClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     final path = Path()..moveTo(0, 0)..lineTo(size.width, 0)..lineTo(size.width, size.height - 24);
-    final wave = Path()..moveTo(size.width, size.height - 24)
+    final wave = Path()
+      ..moveTo(size.width, size.height - 24)
       ..cubicTo(size.width * .88, size.height - 8, size.width * .74, size.height - 34, size.width * .58, size.height - 18)
       ..cubicTo(size.width * .43, size.height - 2, size.width * .27, size.height - 35, 0, size.height - 13);
-    path.addPath(wave, Offset.zero)..close();
+    path.addPath(wave, Offset.zero);
+    path.close();
     return path;
   }
   @override

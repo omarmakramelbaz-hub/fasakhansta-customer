ef7b03df7afcf676484e39061db3098c538541ff
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-22 00:12:53 +0300

    Reduce home header height to 100

--- lib/view/layout/home/widgets/home_header.dart
+++ lib/view/layout/home/widgets/home_header.dart
@@ -31,7 +31,7 @@ class HomeHeader extends StatelessWidget implements PreferredSizeWidget {
     return ClipPath(
       clipper: const _FiveAngleHeaderClipper(),
       child: Container(
-        height: 160,
+        height: 100,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topLeft,
@@ -122,7 +122,7 @@ class HomeHeader extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(160);
+  Size get preferredSize => const Size.fromHeight(100);
 }
 
 class _LocationButton extends StatelessWidget {

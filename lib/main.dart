599833b8dee14796b45825604b50b7ab78bc0567
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-28 01:41:04 +0300

    Initialize Facebook authentication on web

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 import 'helpers/app/app.dart';
 import 'helpers/app/init_services.dart';
 
+const String _facebookAppId = String.fromEnvironment('FACEBOOK_APP_ID');
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb && _facebookAppId.isNotEmpty) {
+    await FacebookAuth.i.webAndDesktopInitialize(
+      appId: _facebookAppId,
+      cookie: true,
+      xfbml: true,
+      version: 'v21.0',
+    );
+  }
+
   await initServices();
   runApp(const MyApp());
 }

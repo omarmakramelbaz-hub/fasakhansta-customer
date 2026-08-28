a9ad8540663840eb4d4c018b6e632dd2868312bd
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-28 05:23:38 +0300

    Keep mobile opening video fallback

--- lib/view/layout/on_boarding/screen/opening_video_controller_stub.dart
+++ lib/view/layout/on_boarding/screen/opening_video_controller_stub.dart
@@ -1,7 +1,16 @@
+import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 
 Future<VideoPlayerController> createOpeningVideoController(
   List<String> chunkPaths,
-) {
-  throw UnsupportedError('Opening video is not supported on this platform.');
+) async {
+  final chunks = await Future.wait(chunkPaths.map(rootBundle.loadString));
+  final encoded = chunks
+      .map((chunk) => chunk.replaceAll(RegExp(r'\s+'), ''))
+      .join();
+
+  return VideoPlayerController.networkUrl(
+    Uri.parse('data:video/mp4;base64,$encoded'),
+    videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
+  );
 }

1f9266d8f83596364de08a064cec9d9bdf7c830b
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-29 05:20:31 +0300

    Match previous products cards to best sellers

--- lib/view/layout/restaurants/widgets/previous_orders_list_view_widget.dart
+++ lib/view/layout/restaurants/widgets/previous_orders_list_view_widget.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../helpers/routes/app_routers_import.dart';
+import '../../../../helpers/theme/app_colors.dart';
 import '../../../../helpers/theme/app_text_style.dart';
 import '../../../../helpers/translation/all_translation.dart';
 import '../../../../helpers/utils/common_methods.dart';
 import '../../../custom_widgets/custom_image/custom_network_image.dart';
 import '../controller/restaurants_controller.dart';
+import '../model/previous_order_model.dart';
 import '../screen/product_details_screen.dart';
 
 class PreviousOrdersListViewWidget extends StatelessWidget {
@@ -15,66 +17,122 @@ class PreviousOrdersListViewWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<RestaurantsController>(
-      builder: (BuildContext context, restaurantsController, _) => Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: SizedBox(
-          height: 144,
-          child: ListView.builder(
+      builder: (context, restaurantsController, _) {
+        return SizedBox(
+          height: 212,
+          child: ListView.separated(
             scrollDirection: Axis.horizontal,
-            shrinkWrap: true,
             itemCount: restaurantsController.previousOrders.length,
+            separatorBuilder: (_, __) => const SizedBox(width: 10),
             itemBuilder: (context, index) {
-              return InkWell(
-                onTap: () {
-                  if (restaurantsController.detailsRestaurant?.status == 'closed') {
-                    CommonMethods.showError(message: 'thisRestaurantIsClosed'.tr);
-                  } else {
-                    if (restaurantsController.previousOrders[index].status == 'show') {
-                      NamedNavigatorImpl.push(
-                        ProductDetailsScreen.routeName,
-                        arguments: ProductDetailsDetailsArgs(id: restaurantsController.previousOrders[index].id ?? 0),
-                      );
-                    } else {
-                      CommonMethods.showError(message: 'productNotAvailable'.tr);
-                    }
-                  }
-                },
-                child: Row(
-                  children: [
-                    Column(
-                      children: [
-                        CustomNetworkImage(
-                          imageUrl: restaurantsController.previousOrders[index].productImage ?? '',
-                          height: 84,
-                          width: 116,
-                          radius: 12,
-                          fit: BoxFit.cover,
-                        ),
-                        const SizedBox(height: 12),
-                        Text(
-                          restaurantsController.previousOrders[index].productName ?? '',
-                          style: AppTextStyle.text16RS(),
-                        ),
-                        const SizedBox(height: 8),
-                        if (restaurantsController.previousOrders[index].productPrice != null) ...[
-                          restaurantsController.previousOrders[index].status == 'show'
-                              ? Text(
-                                  'pound'.tr.replaceAll(
-                                        '{}',
-                                        restaurantsController.previousOrders[index].productPrice.toString(),
-                                      ),
-                                  style: AppTextStyle.text14RG(),
-                                )
-                              : Text('notAvailable'.tr, style: AppTextStyle.text16RG()),
-                        ],
-                      ],
-                    ),
-                    const SizedBox(width: 5),
-                  ],
-                ),
+              final item = restaurantsController.previousOrders[index];
+              return _PreviousProductCard(
+                item: item,
+                restaurantClosed:
+                    restaurantsController.detailsRestaurant?.status == 'closed',
               );
             },
           ),
+        );
+      },
+    );
+  }
+}
+
+class _PreviousProductCard extends StatelessWidget {
+  final PreviousOrderModel item;
+  final bool restaurantClosed;
+
+  const _PreviousProductCard({
+    required this.item,
+    required this.restaurantClosed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final available = item.status == 'show';
+
+    return GestureDetector(
+      onTap: () {
+        if (restaurantClosed) {
+          CommonMethods.showError(message: 'thisRestaurantIsClosed'.tr);
+          return;
+        }
+
+        if (!available) {
+          CommonMethods.showError(message: 'productNotAvailable'.tr);
+          return;
+        }
+
+        NamedNavigatorImpl.push(
+          ProductDetailsScreen.routeName,
+          arguments: ProductDetailsDetailsArgs(id: item.id ?? 0),
+        );
+      },
+      child: Container(
+        width: 155,
+        decoration: BoxDecoration(
+          color: AppColors.whiteColor,
+          borderRadius: BorderRadius.circular(18),
+          border: Border.all(color: const Color(0xFFEAEAEA)),
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 112,
+              width: double.infinity,
+              child: CustomNetworkImage(
+                imageUrl: item.productImage ?? '',
+                radius: 0,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 8, 10, 4),
+              child: Text(
+                item.productName ?? '',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: AppTextStyle.text14BS(),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    available
+                        ? '${item.productPrice?.toStringAsFixed(0) ?? '-'} ج'
+                        : 'غير متاح',
+                    style: AppTextStyle.text14BS().copyWith(
+                      color: available
+                          ? AppColors.mainAppColor
+                          : AppColors.greyColor,
+                    ),
+                  ),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: available
+                          ? AppColors.mainAppColor
+                          : AppColors.greyColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 8),
+          ],
         ),
       ),
     );

4299cc3a12950e181ce3ae178f2400120cd0ed03
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-13 01:48:58 +0300

    Move home address block lower by half centimeter

--- lib/view/layout/home/widgets/home_header.dart
+++ lib/view/layout/home/widgets/home_header.dart
@@ -37,7 +37,7 @@ class HomeHeader extends StatelessWidget implements PreferredSizeWidget {
         child: SafeArea(
           bottom: false,
           child: Padding(
-            padding: const EdgeInsets.fromLTRB(12, 45, 12, 0),
+            padding: const EdgeInsets.fromLTRB(12, 63, 12, 0),
             child: Directionality(
               textDirection: TextDirection.ltr,
               child: GestureDetector(
